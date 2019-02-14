95ad8c3397c58bbd76cb3ec8c6060a12c471924d
Andrew Brown <ubw@kmd.dk>
2019-02-14 16:01:45 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# cosmosdb-backup
+
+Backup and restore a CosmosDB database to or from files.
+
+The files should be named `collection name`.cosmosbak and contain an array of JSON objects.
+
+Command line options:
+
+```
+  -a, --action                  Required. The action to perform (Backup or Restore)
+
+  -c, --connectionstring        Required. The connection string to CosmosDb
+
+  -d, --databasename            Required. The database name to backup or restore
+
+  -p, --partitionkey            The partition key field
+
+  -k, --defaultkey              The default partition key if not present in the document
+
+  -f, --folder                  The folder used for the backup or restore. Defaults to the current directory.
+
+  -t, --databasethroughput      The throughput (RUs) for the database when being created.
+
+  -r, --collectionthroughput    The throughput (RUs) for the collection when being created.
+
+  --help                        Display this help screen.
+
+  --version                     Display version information.
+```
---
+++ src/cosmosdb-backup/Options.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+
+namespace CosmosBackup
+{
+    public enum CosmosAction
+    {
+        /// <summary>
+        /// Backup the database to files
+        /// </summary>
+        Backup,
+
+        /// <summary>
+        /// Restore the database from files
+        /// </summary>
+        Restore
+    }
+
+    public class Options
+    {
+        [Option('a', "action", Required = true, HelpText = "The action to perform (Backup or Restore)")]
+        public CosmosAction Action { get; set; }
+
+        [Option('c', "connectionstring", Required = true, HelpText = "The connection string to CosmosDb")]
+        public string ConnectionString { get; set; }
+
+        [Option('d', "databasename", Required = true, HelpText = "The database name to backup or restore")]
+        public string Database { get; set; }
+
+        [Option('p', "partitionkey", Required = false, HelpText = "The partition key field")]
+        public string PartitionKey { get; set; }
+
+        [Option('k', "defaultkey", Required = false, HelpText = "The default partition key if not present in the document")]
+        public string PartitionKeyDefault { get; set; }
+
+        [Option('f', "folder", Required = false, HelpText = "The folder used for the backup or restore. Defaults to the current directory.")]
+        public string Folder { get; set; }
+
+        [Option('t', "databasethroughput", Required = false, HelpText = "The throughput (RUs) for the database when being created.")]
+        public int? DatabaseThroughput { get; set; }
+
+        [Option('r', "collectionthroughput", Required = false, HelpText = "The throughput (RUs) for the collection when being created.")]
+        public int? CollectionThroughput { get; set; }
+    }
+}
